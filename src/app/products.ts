eb39dacb652f2a7ba9ba435ccdb35cbcfedb64a3
alfredocelso <alfredo.celso@mercer.com>
2019-10-15 14:13:04 +0200

    Jenkins tutorial

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Market Pricing - Total Cash',
+    price: 699,
+    description: 'Market Pricing Pro is a reliable and cost-effective report covering information on position/job remuneration in the marketplace. This analysis enables clients to guide remuneration for new recruits or calculate next years salary increase budgets.'
+  },
+  {
+    name: 'Mobility: Other Analysis',
+    price: 699,
+    description: 'Various billable projects from Mobility area that would included more customized approach and advance analytical support.'
+  },
+  {
+    name: 'Compensation Planning/Pulse',
+    price: 699,
+    description: 'Global Online Surveys Team offers a great way of quickly reaching and engaging clients’ audience.'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
